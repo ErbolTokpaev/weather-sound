eaee4fea38c52d3bbaeb9809e065395d27ce8285
etokpaev <erbol.tokpaev@gmail.com>
2023-10-26 13:49:30 +0600

    feat: added pause functionality

---
+++ src/data.js
@@ -0,0 +1,15 @@
+const data = [
+    { id: 1, icon: 'sun', bg: 'summer', sound: 'summer'},
+    { id: 2, icon: 'cloud-rain', bg: 'rainy', sound: 'rain'},
+    { id: 3, icon: 'cloud-snow', bg: 'winter', sound: 'winter'},
+];
+const seasonIcons = {
+    summer: 'sun',
+    rain: 'cloud-rain',
+    winter: 'cloud-snow',
+}
+
+export {
+    data,
+    seasonIcons
+}
--- src/index.js
+++ src/index.js
@@ -1,69 +1,98 @@
 import './index.scss'
-const data = [
-    { id: 1, icon: 'sun', bg: 'summer', sound: 'summer'},
-    { id: 2, icon: 'cloud-rain', bg: 'rainy', sound: 'rain'},
-    { id: 3, icon: 'cloud-snow', bg: 'winter', sound: 'winter'},
-]
+import { data, seasonIcons } from './data';
+
+let VOL = -1;
+let isPaused = false
+
 const root = document.getElementById('app');
+const weatherBlock  = document.createElement('div');
+weatherBlock.className = 'weather-block'
 
-const block  = document.createElement('div');
-block.className = 'weather-block'
-root.append(block);
+const volumeChanger = document.createElement('input')
+volumeChanger.setAttribute('type', 'range')
+volumeChanger.setAttribute('min', '0');
+volumeChanger.setAttribute('max', '100');
+volumeChanger.setAttribute('value', '100')
+volumeChanger.addEventListener('input', () => changeVolume())
 
-let vol = -1;
-
+root.append(weatherBlock);
+root.append(volumeChanger);
 
 function createContent(item) {
     const div = document.createElement('div')
     div.classList.add('weather-block-item')
+    div.id = item.sound
     div.addEventListener('click', () => runSound(item))
     div.style.backgroundImage = 'url(\'./assets/' + item.bg + '-bg.jpg\')';
     const icon = document.createElement('img')
     icon.setAttribute('src', './assets/icons/' + item.icon + '.svg')
     div.append(icon);
-
-    block.append(div)
+    weatherBlock.append(div)
 }
-
-data.forEach(createContent)
-
-const volume = document.createElement('input')
-volume.setAttribute('type', 'range')
-volume.setAttribute('min', '0');
-volume.setAttribute('max', '100');
-volume.setAttribute('value', '100')
-volume.addEventListener('input', () => changeVolume())
-
-root.append(volume);
 
 function runSound(item) {
     const body = document.getElementById('body');
     body.style.backgroundImage ='url(\'./assets/' + item.bg + '-bg.jpg\')';
-    const isAudioExist = document.getElementsByTagName('audio');
-    if (isAudioExist.length !== 0) { 
-        isAudioExist[0].remove()
-    }
-    const audio = document.createElement('audio');
+    const isAudioExist = document.getElementById('audio');
 
-    const source = document.createElement('source');
-    audio.setAttribute('loop', true);
-    audio.id = 'audio';
-    source.setAttribute('src', `./assets/sounds/${item.sound}.mp3`);
-    source.setAttribute('type', 'audio/mpeg');
-    if (vol > -1) {
-        audio.volume = vol
+    if (isAudioExist) { 
+        if (isAudioExist.getAttribute('name') === item.sound) {
+            if (isPaused) {
+                isAudioExist.play();
+                isPaused = false;
+                changeIcon(item.sound, 'pause')
+            } else {
+                isAudioExist.pause();
+                isPaused = true;
+                changeIcon(item.sound, item.icon)
+            }
+        } else {
+            changeIcon(isAudioExist.getAttribute('name'), seasonIcons[isAudioExist.getAttribute('name')])
+            isPaused = false;
+            isAudioExist.remove();
+            createAudioEl(item.sound);
+            changeIcon(item.sound, 'pause')
+        }
+       
+    } else {
+        createAudioEl(item.sound);
+        changeIcon(item.sound, 'pause')
     }
-    audio.append(source);
-    block.append(audio)
-    audio.play();
+    
 }
 
 function changeVolume() {
-    const newVolume = volume.value;
+    const newVolume = volumeChanger.value;
     const audio = document.getElementById('audio')
-    vol = newVolume/100;
+    VOL = newVolume/100;
     if (audio) {
         audio.volume = newVolume/100;
     }
 
 }
+
+function changeIcon(sound, icon) {
+    const season = document.getElementById(sound)
+    season.removeChild(season.firstChild)
+    const img = document.createElement('img')
+    img.setAttribute('src', './assets/icons/' + icon +'.svg')
+    season.append(img);
+}
+
+function createAudioEl(sound) {
+    const audio = document.createElement('audio');
+    const source = document.createElement('source');
+    audio.setAttribute('loop', true);
+    audio.id = 'audio';
+    audio.setAttribute('name', sound);
+    source.setAttribute('src', `./assets/sounds/${sound}.mp3`);
+    source.setAttribute('type', 'audio/mpeg');
+    if (VOL > -1) {
+        audio.volume = VOL
+    }
+    audio.append(source);
+    weatherBlock.append(audio)
+    audio.play();
+}
+
+data.forEach(createContent)
